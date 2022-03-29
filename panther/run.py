a9b5c3091d441a67e66af9a8905b89a9357fd073
wezham <wesley.hamburger@afterpay.com>
2022-03-30 08:15:46 +1100

    Modify to include creating global helpers

--- panther/run.py
+++ panther/run.py
@@ -2,7 +2,9 @@ import argparse
 import pathlib
 import pkgutil
 import sys
+from enum import Enum
 from logging import DEBUG, Formatter, StreamHandler, getLogger
+from typing import Dict, List, NamedTuple
 
 logger = getLogger(__name__)
 logger.setLevel(DEBUG)
@@ -13,19 +15,52 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-def _create_dest_file_at_path(
-    target_dir: pathlib.Path, sample_file_name: str, dest_filename: str
-):
-    """Copies the contents of the sample file to the destiantion file in the target directory."""
-    sample_file_contents = pkgutil.get_data(
-        __name__, f"data/{sample_file_name}"
-    ).decode()
-    dest_file = target_dir.joinpath(dest_filename)
-    logger.debug(f"Creating python file at {dest_file}")
-    with open(dest_file, "w") as f:
-        f.write(sample_file_contents)
+class FileType(Enum):
+    helper_py = "sample_helper.py"
+    helper_yml = "sample_helper.yml"
+    query = "sample_query.yml"
+    rule_yml = "sample_rule.yml"
+    rule_py = "sample_rule_py.py"
+    scheduled_rule = "sample_schedule_rule.yml"
 
-    logger.debug("Successfully wrote new file")
+
+class FileInformation(NamedTuple):
+    extension: str
+    filename_suffix: str = ""
+
+
+FILE_TYPE_TO_FILE_INFORMATION: Dict[FileType, FileInformation] = {
+    FileType.helper_py: FileInformation(extension="py"),
+    FileType.helper_yml: FileInformation(extension="yml"),
+    FileType.query: FileInformation(extension="yml", filename_suffix="_query"),
+    FileType.rule_yml: FileInformation(extension="yml"),
+    FileType.rule_py: FileInformation(extension="py"),
+    FileType.scheduled_rule: FileInformation(extension="yml"),
+}
+
+
+def create_files_for_item_type(
+    file_types: List[FileType], target_dir: pathlib.Path, target_filename: str
+) -> None:
+    """Creates"""
+    for file_type in file_types:
+        # Get the sample file to generate the new file
+        sample_file_contents = pkgutil.get_data(
+            __name__, f"data/{file_type.value}"
+        ).decode()
+
+        # Get information about the file to create
+        file_information = FILE_TYPE_TO_FILE_INFORMATION[file_type]
+        file_to_create = (
+            f"{target_filename}{file_information.filename_suffix}."
+            f"{file_information.extension}"
+        )
+        dest_file = target_dir.joinpath(file_to_create)
+
+        # Write file to destination
+        logger.debug(f"Creating python file at {dest_file}")
+        with open(dest_file, "w") as f:
+            f.write(sample_file_contents)
 
 
 def main():
@@ -45,8 +80,8 @@ def main():
         metavar="item-type",
         type=str,
         nargs="?",
-        help="The type of the of the detection you would like to create [rule,scheduled-rule]",
-        choices=["rule", "scheduled-rule"],
+        help="What you would like to create [rule,scheduled-rule,helper]",
+        choices=["rule", "scheduled-rule", "helper"],
         required=True,
     )
 
@@ -60,39 +95,21 @@ def main():
     )
 
     args = parser.parse_args()
-    print(args)
     target_dir_path = pathlib.Path(args.target_dir)
-    if args.item == "rule":
-        # A rule requires a python file, a rule file
-        _create_dest_file_at_path(
-            target_dir=target_dir_path,
-            sample_file_name="sample_py.py",
-            dest_filename=f"{args.file_name}.py",
-        )
 
-        _create_dest_file_at_path(
-            target_dir=target_dir_path,
-            sample_file_name="sample_rule.yml",
-            dest_filename=f"{args.file_name}.yml",
-        )
+    if args.item == "rule":
+        # A rule requires a python file and a yaml file
+        file_types = [FileType.rule_py, FileType.rule_yml]
     elif args.item == "scheduled-rule":
         # A scheduled rule requires a python file, a scheduled_query and a scheduled_rule
-        _create_dest_file_at_path(
-            target_dir=target_dir_path,
-            dest_filename=f"{args.file_name}.py",
-            sample_file_name="sample_py.py",
-        )
-
-        _create_dest_file_at_path(
-            target_dir=target_dir_path,
-            dest_filename=f"{args.file_name}.yml",
-            sample_file_name="sample_scheduled_rule.yml",
-        )
-
-        _create_dest_file_at_path(
-            target_dir=target_dir_path,
-            dest_filename=f"{args.file_name}_query.yml",
-            sample_file_name="sample_query.yml",
-        )
+        file_types = ([FileType.rule_py, FileType.scheduled_rule, FileType.query],)
+    elif args.item == "helper":
+        file_types = [FileType.helper_py, FileType.helper_yml]
     else:
         raise ValueError(f"{args.item} not supported")
+
+    create_files_for_item_type(
+        file_types=file_types,
+        target_dir=target_dir_path,
+        target_filename=args.file_name,
+    )
