b413ebd4196f60ca1e13e8afbee1e8065c42a1d1
wezham <wesley.hamburger@afterpay.com>
2022-04-04 14:15:17 +1000

    add integration tests and unit tests

--- tests/unit/test_file_content_modifiers.py
+++ tests/unit/test_file_content_modifiers.py
@@ -8,4 +8,4 @@ def test_filename_to_rule_name():
     assert filename_to_rule_name('aws_iam_secret_access') == "Rule.Aws.Iam.Secret.Access"
 
 def test_filename_to_python_file():
-    assert filename_to_rule_name('aws_iam_secret_access') == "aws_iam_secret_access.py"
+    assert filename_to_python_file('aws_iam_secret_access') == "aws_iam_secret_access.py"
