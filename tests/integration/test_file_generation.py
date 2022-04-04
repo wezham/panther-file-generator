5151d2ed3da2c2e910f6e879848808eaf71091f5
wezham <wesley.hamburger@afterpay.com>
2022-04-04 14:15:33 +1000

    black on tests

--- tests/integration/test_file_generation.py
+++ tests/integration/test_file_generation.py
@@ -12,11 +12,14 @@ def test_create_files_for_item_type_successfully_creates_files():
     with TemporaryDirectory() as tmpdir:
         path_to_files = pathlib.Path(tmpdir)
         create_files_for_item_type(
-            file_types=files_to_create, target_dir=path_to_files, target_filename="testing_it"
+            file_types=files_to_create,
+            target_dir=path_to_files,
+            target_filename="testing_it",
         )
 
         assert path_to_files.joinpath("testing_it.py").exists()
         assert path_to_files.joinpath("testing_it.yml").exists()
+
 
 def test_create_files_for_item_type_creates_no_files_if_errors():
     files_to_create = [FileType.helper_py, "bad argument"]
@@ -25,12 +28,9 @@ def test_create_files_for_item_type_creates_no_files_if_errors():
         path_to_files = pathlib.Path(tmpdir)
         with pytest.raises(Exception):
             create_files_for_item_type(
-                file_types=files_to_create, target_dir=path_to_files, target_filename="testing_it"
+                file_types=files_to_create,
+                target_dir=path_to_files,
+                target_filename="testing_it",
             )
 
         assert not path_to_files.joinpath("testing_it.py").exists()
-
-
-
-
-
